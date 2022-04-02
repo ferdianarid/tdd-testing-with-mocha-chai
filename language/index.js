1f53cc9e9a4af5b157b94ccc40eb53ddf0498990
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-04-02 16:01:45 +0700

    create dummy fake data object

---
+++ language/index.js
@@ -0,0 +1,33 @@
+const programmingLanguage = [{
+        id: 1,
+        title: "JavaScript Language",
+        popularity: 737463568,
+        created_at: new Date(Date.UTC(0, 0, 0, 7, 0, 0))
+    },
+    {
+        id: 2,
+        title: "Python Languages",
+        popularity: 593459365,
+        created_at: new Date(Date.UTC(0, 0, 0, 7, 0, 0))
+    },
+    {
+        id: 3,
+        title: "Elixir Language",
+        popularity: 937247547,
+        created_at: new Date(Date.UTC(0, 0, 0, 7, 0, 0))
+    },
+    {
+        id: 4,
+        title: "TypeScript Language",
+        popularity: 944587263,
+        created_at: new Date(Date.UTC(0, 0, 0, 7, 0, 0))
+    },
+    {
+        id: 5,
+        title: "Swift Language",
+        popularity: 267548566,
+        created_at: new Date(Date.UTC(0, 0, 0, 7, 0, 0))
+    }
+]
+
+module.exports = programmingLanguage
